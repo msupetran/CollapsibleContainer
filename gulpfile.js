342f12686e3c5493601179ef8b2146455f419b46
Marlon Supetran <msupetran@blastasia.com>
2016-07-10 00:32:34 +0800

    Fixed: gulp-inject settings.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
 var gulp = require('gulp');
 var gulpInject = require('gulp-inject');
 var browserSync = require('browser-sync').create();
+var paths = {
+    index: './src/index,html',
+    scripts: './src/**/*.js',
+    styles: './src/**.*.css'
+};
 
 // Tasks
 gulp.task('inject', inject);
@@ -10,16 +15,20 @@ gulp.task('watch', ['inject'], reload);
 
 // Methods
 function inject() {
-    var target = gulp.src('./src/index.html');
+    var target = gulp.src(paths.index);
     var sources = gulp.src([
-        './src/**/*.js',
-        './src/**/*.css'
+        paths.scripts,
+        paths.styles
     ],
         { read: false }
     );
+    var options = {
+        ignorePath: 'src',
+        addRootSlash: false
+    };
 
     return target
-        .pipe(gulpInject(sources))
+        .pipe(gulpInject(sources, options))
         .pipe(gulp.dest('./src'));
 }
 function launch() {
